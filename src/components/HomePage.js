a6aaffd73f4faf3475fc6b905b1e244602a73196
apuravdhiman80 <140954620+apuravdhiman80@users.noreply.github.com>
2024-12-23 18:03:18 +0530

    okok

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./HomePage.css";
-
+const styles = {
+  display: "flex",
+  justifyContent: "center",
+  marginTop: "100px",
+};
 function HomePage() {
   const navigate = useNavigate();
 
@@ -9,10 +13,15 @@ function HomePage() {
     <div className="homepage">
       <h1>Prim's Algorithm Visualizer</h1>
       <p>
-        Prim's Algorithm is a greedy algorithm that builds a minimum spanning tree (MST)
-        by adding the smallest edge at each step while avoiding cycles.
+        Prim's Algorithm is a greedy algorithm that builds a minimum spanning
+        tree (MST) by adding the smallest edge at each step while avoiding
+        cycles.
       </p>
-      <button onClick={() => navigate("/visualizer")}>Start Visualizer</button>
+      <div style={styles}>
+        <button onClick={() => navigate("/visualizer")}>
+          Start Visualizer
+        </button>
+      </div>
     </div>
   );
 }
